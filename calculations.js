950fe5c757dd0d7f51acb023c4c8f64404d48797
Milena Angelova <milene.angelova@gmail.com>
2023-05-14 20:59:47 +0300

    Infinity added

--- calculations.js
+++ calculations.js
@@ -2,18 +2,10 @@ let displayValue = '0';
 let firstOperand = null;
 let secondOperand = null;
 let operator = null;
-// let secondOperator = null;
 let result = null;
 
 const buttons = document.querySelectorAll('button');
 const display = document.getElementById('display');
-
-debugger;
-
-// document.addEventListener('keydown', function (e) {
-//     const key = document.querySelector(`button[data-key='${e.key}']`);
-//     key.click();
-// });
 
 function addition(firstNumber, secondNumber) {
     return (firstNumber + secondNumber);
@@ -29,7 +21,7 @@ function multiplication(firstNumber, secondNumber) {
 
 function division(firstNumber, secondNumber) {
     if (secondNumber === 0) {
-        return 'Can not divide by zero';
+        return 'Infinity';
     }
     return (firstNumber / secondNumber);
 }
@@ -57,23 +49,19 @@ function clickButton() {
         buttons[i].addEventListener('click', function () {
                 if (buttons[i].classList.contains('operand')) {
                     inputOperand(buttons[i].value);
-                    updateDisplay();
                 } else if (buttons[i].classList.contains('operator')) {
                     inputOperator(buttons[i].value);
                 } else if (buttons[i].classList.contains('equals')) {
                     inputEquals();
-                    updateDisplay();
                 } else if (buttons[i].classList.contains('decimal')) {
                     inputDecimal(buttons[i].value);
-                    updateDisplay();
                 } else if (buttons[i].classList.contains('backspace')) {
                     backspace(displayValue);
-                    updateDisplay();
                 } else if (buttons[i].classList.contains('sign')) {
                     inputSign(displayValue);
-                    updateDisplay();
-                } else if (buttons[i].classList.contains('clear'))
+                } else if (buttons[i].classList.contains('clear')) {
                     clearDisplay();
+                }
                 updateDisplay();
             }
         )
@@ -85,16 +73,13 @@ clickButton();
 function inputOperand(operand) {
     if (operator === null) {
         if (displayValue === '0' || displayValue === 0) {
-            //1st click - handles first operand input
             displayValue = operand;
         } else if (displayValue === firstOperand) {
-            //starts new operation after inputEquals()
             displayValue = operand;
         } else {
             displayValue += operand;
         }
     } else {
-        //3rd/5th click - inputs to secondOperand
         if (displayValue === firstOperand) {
             displayValue = operand;
         } else {
@@ -134,20 +119,11 @@ function inputOperator(currentOperator) {
 }
 
 function inputEquals() {
-    //hitting equals doesn't display undefined before operate()
     if (operator === null) {
         return;
     }
-    // else if (operator != null) {
-    //handles final result
     secondOperand = displayValue;
     result = operate(Number(firstOperand), Number(secondOperand), operator);
-    //     checkResult();
-    // } else {
-    //     //handles first operation
-    //     secondOperand = displayValue;
-    //     result = operate(Number(firstOperand), Number(secondOperand), operator);
-    // }
     checkResult();
 }
 
@@ -169,7 +145,6 @@ function clearDisplay() {
     firstOperand = null;
     secondOperand = null;
     operator = null;
-    secondOperator = null;
     result = null;
 }
 
@@ -194,12 +169,13 @@ function roundAccurately(number, places) {
 function checkResult() {
     if (result === 0) {
         displayValue = 0;
+    } else if (result === 'Infinity') {
+        displayValue = 'Infinity';
     } else {
         displayValue = roundAccurately(result, 4).toString();
         firstOperand = displayValue;
         secondOperand = null;
         operator = null;
-        secondOperator = null;
-        result = null;
+        result = '';
     }
 }
